8dc9023b9553ec332016a47a5f4e3a8fe46ca444
Kuhn Chris <kuhnchris@kuhnchris.eu>
2021-11-23 17:22:43 +0100

    Initial commit to work with `act`

---
+++ .github/actions/fetchGodot/entrypoint.sh
@@ -0,0 +1,19 @@
+
+#!/bin/sh
+targetDir="/github/workspace/"
+#set -v -x
+
+cd $targetDir;
+if [ "$BASE_DIR" != "" ]; then
+    targetDir="/github/workspace/${BASE_DIR}/"
+fi
+
+wget https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_export_templates.tpz -O templ.tpz
+wget https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_x11.64.zip -O app.zip
+unzip app.zip
+chmod +x godot*
+mkdir ~/.local/share/godot/templates -p
+tar xf templ.tpz
+mv templates ~/.local/share/godot/${GODOT_VERSION}.stable
+
+#set +v +x
---
+++ README.md
@@ -0,0 +1,5 @@
+# GitHub CI Integration for Godot (auto-build)
+
+Example project to test is in `example-project/`
+
+Developed with ❤️ using `act` (https://github.com/nektos/act)
