41b8aa2c5cade98de5c1563f498ae03daecd3424
Kuhn Chris <kuhnchris@kuhnchris.eu>
2021-11-23 18:11:44 +0100

    Added debug

--- .github/actions/fetchGodot/entrypoint.sh
+++ .github/actions/fetchGodot/entrypoint.sh
@@ -12,8 +12,9 @@ cd $targetDir;
 if [ "$BASE_DIR" != "" ]; then
     targetDir="/github/workspace/${BASE_DIR}/"
 fi
-
+echo "downloading templates... https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_export_templates.tpz"
 wget https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_export_templates.tpz -O templ.tpz
+echo "downloading engine... https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_x11.64.zip"
 wget https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_x11.64.zip -O app.zip
 unzip app.zip
 chmod +x godot*
