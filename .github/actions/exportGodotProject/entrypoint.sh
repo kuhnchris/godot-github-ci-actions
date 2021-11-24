1b28a9f5ca5c9da5a850248a7e2d8e8a777b6349
Kuhn Chris <kuhnchris@kuhnchris.eu>
2021-11-24 19:29:50 +0100

    itteration #1

---
+++ .github/actions/exportGodotProject/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#set -v -x
+targetDir=${GITHUB_WORKSPACE}
+BASE_DIR=$1
+DEBUG=$2
+PACK=3
+PLATFORM=4
+if [ "$BASE_DIR" != "" ]; then
+    targetDir="${GITHUB_WORKSPACE}/${BASE_DIR}/"
+fi
+
+rm -Rf ./export-debug 2>&1| true
+rm -Rf ./export-pck 2>&1| true
+rm -Rf ./export-platform 2>&1| true
+mkdir export-debug
+mkdir export-pck
+mkdir export-platform
+
+godot_args=""
+if [ "${DEBUG}x" == "x" ]; then
+    godot_args=${godot_args} --export-debug ${PLATFORM} ./export-debug    
+fi
+
+if [ "${PACK}x" == "x" ]; then
+    godot_args=${godot_args} --export-pack ${PLATFORM} ./export-pck    
+fi
+
+if [ "${PLATFORM}x" == "x" ]; then
+    godot_args=${godot_args} --export ${PLATFORM} ./export-platform    
+fi
+
+./Godot* ${godot_args} --no-window
+#set +v +x
--- .github/actions/fetchGodot/entrypoint.sh
+++ .github/actions/fetchGodot/entrypoint.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
-targetDir=$GITHUB_WORKSPACE
 #set -v -x
+targetDir=${GITHUB_WORKSPACE}
 GODOT_VERSION=$1
+TEMPLATE_TARGET${TEMPLATE_TARGET}/${GODOT_VERSION}.stable
+TEMPLATE_BASE=~/.local/share/godot/templates
+URL_PREFIX=https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_
 echo "::debug::running with parameters: $GITHUB_WORKSPACE - $GODOT_VERSION" 
 if [ "$GODOT_VERSION" == "" ]; then
     echo "no Godot Version defined. cannot proceed."
     exit 2
 fi  
+cd ${targetDir};
 
-cd $targetDir;
-if [ "$BASE_DIR" != "" ]; then
-    targetDir="/github/workspace/${BASE_DIR}/"
-fi
-echo "downloading templates... https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_export_templates.tpz"
-wget https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_export_templates.tpz -O templ.tpz
-echo "downloading engine... https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_x11.64.zip"
+echo "::debug::using this url prefix: ${URL_PREFIX}"
+echo "downloading templates... export_templates.tpz"
+wget ${URL_PREFIX}export_templates.tpz -O templ.tpz
+echo "downloading engine... x11.64.zip"
 wget https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_x11.64.zip -O app.zip
+echo "::debug::unzipping engine zip file..."
 unzip app.zip
-chmod +x godot*
-mkdir ~/.local/share/godot/templates -p
+echo "::debug::make engine executable..."
+chmod +x Godot*
+echo "::debug::creating template directory..."
+mkdir ${TEMPLATE_BASE} -p -v
+echo "::debug::unzipping template zip..."
 unzip templ.tpz
-mv templates ~/.local/share/godot/${GODOT_VERSION}.stable
+echo "::debug::moving templates to ${TEMPLATE_TARGET}"
+mv -v templates ${TEMPLATE_TARGET}
 
 #set +v +x
