e28eb189546e3a1c2096195311d70395d44c2a23
Kuhn Chris <kuhnchris@kuhnchris.eu>
2021-11-24 21:33:31 +0100

    Added parameters to action.yml

--- .github/actions/exportGodotProject/entrypoint.sh
+++ .github/actions/exportGodotProject/entrypoint.sh
@@ -17,7 +17,8 @@ targetDir=${GITHUB_WORKSPACE}
 BASE_DIR=$1
 DEBUG=$2
 PACK=$3
-PLATFORM=$4
+PLATFORM_EXPORT=$4
+PLATFORM=$5
 if [ "$BASE_DIR" != "" ]; then
     targetDir="${GITHUB_WORKSPACE}/${BASE_DIR}/"
     echo "Using this project directory: ${targetDir}"
@@ -31,15 +32,15 @@ mkdir export-pck
 mkdir export-platform
 
 godot_args=""
-if [ "${DEBUG}x" == "x" ]; then
+if [ "${DEBUG}x" != "x" ]; then
     godot_args="${godot_args} --export-debug ${PLATFORM} ./export-debug"
 fi
 
-if [ "${PACK}x" == "x" ]; then
+if [ "${PACK}x" != "x" ]; then
     godot_args="${godot_args} --export-pack ${PLATFORM} ./export-pck"    
 fi
 
-if [ "${PLATFORM}x" == "x" ]; then
+if [ "${PLATFORM_EXPORT}x" != "x" ]; then
     godot_args="${godot_args} --export ${PLATFORM} ./export-platform"
 fi
 
