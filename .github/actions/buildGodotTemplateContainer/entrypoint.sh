80f9d7a67e0ff397c508a18ee1f347d9e64e5759
Kuhn Chris <kuhnchris@kuhnchris.eu>
2021-11-25 14:43:30 +0100

    enter action path first.

--- .github/actions/buildGodotTemplateContainer/entrypoint.sh
+++ .github/actions/buildGodotTemplateContainer/entrypoint.sh
@@ -4,8 +4,8 @@ TAG=godot-github-ci-actions
 #GODOT_VERSION=$1
 #GODOT_VERSION=INPUT_GODOT_VERSION
 
+cd "${ACTION_PATH}" || exit 2;
 sed 's/\$1/'${GODOT_VERSION}'/g' -i fetchGodot/entrypoint.sh
-
 cd fetchGodot || exit 2;
 docker build . --file Dockerfile --tag "${TAG}" --label "runnumber=${GITHUB_RUN_ID}"
 cd ..
