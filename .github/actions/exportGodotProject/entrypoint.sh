6c79b5184c9a91d319acf1ae4be4f9a67d4785e7
Kuhn Chris <kuhnchris@kuhnchris.eu>
2021-11-25 01:40:15 +0100

    Fixed exporting issue

--- .github/actions/exportGodotProject/entrypoint.sh
+++ .github/actions/exportGodotProject/entrypoint.sh
@@ -13,46 +13,58 @@ echo "command line:"
 echo $@
 echo "::endgroup::"
 
+echo "::group::Parsing and preparing variables"
+
 targetDir=${GITHUB_WORKSPACE}
 BASE_DIR=$1
 DEBUG=$2
 PACK=$3
 PLATFORM_EXPORT=$4
 PLATFORM=$5
+EXECNAME=$6
 if [ "$BASE_DIR" != "" && "$BASE_DIR" != "false" ]; then
     targetDir="${GITHUB_WORKSPACE}/${BASE_DIR}/"
     echo "Using this project directory: ${targetDir}"
 fi
 
-rm -Rf ./export-debug 2>&1| true
-rm -Rf ./export-pck 2>&1| true
-rm -Rf ./export-platform 2>&1| true
-rm -Rf ./export-artifacts 2>&1 | true
-mkdir export-debug
-mkdir export-pck
-mkdir export-platform
-mkdir export-artifacts
+targetDirDebug=${targetDir}/export-debug/
+targetDirPck=${targetDir}/export-pck/
+targetDirPlatform=${targetDir}/export-platform/
+echo "::endgroup::"
 
+echo "::group::Cleaning and preparing export directories..."
+rm -Rf ${targetDirDebug} 2>&1| true
+mkdir ${targetDirDebug}
+rm -Rf ${targetDirPck} 2>&1| true
+mkdir ${targetDirPck}
+rm -Rf ${targetDirPlatform} 2>&1| true
+mkdir ${targetDirPlatform}
+rm -Rf ./export-artifacts 2>&1 | true
+echo "::endgroup::"
+
+echo "::group::Evaluating input variables..."
 godot_args=""
 ziping=""
 zippostfix="`date "+automated_build-%Y.%m.%d-%H:%M:%S"`-$GITHUB_REF"
-if [ "${DEBUG}x" != "x" && "${DEBUG}x" != "falsex" ]; then
-    godot_args="${godot_args} --export-debug ${PLATFORM} ./export-debug"
-    ziping="; zip -0 -r export-artifacts/export-with-debug-symbols-${zippostfix}.zip ./export-debug ;"
+if [ "${DEBUG}x" != "x" ] && [ "${DEBUG}x" != "falsex" ]; then
+    godot_args="${godot_args} --export-debug ${PLATFORM} ${targetDirDebug}/${EXECNAME}"
+    ziping="; zip -0 -r export-artifacts/export-with-debug-symbols-${zippostfix}.zip ${targetDirDebug};"
 fi
 
-if [ "${PACK}x" != "x"  && "${PACK}x" != "falsex" ]; then
-    godot_args="${godot_args} --export-pack ${PLATFORM} ./export-pck/game.pck"    
-    ziping="; zip -0 -r export-artifacts/export-pack-${zippostfix}.zip ./export-pck;"
+if [ "${PACK}x" != "x" ]  && [ "${PACK}x" != "falsex" ]; then
+    godot_args="${godot_args} --export-pack ${PLATFORM} ${targetDirPck}/${EXECNAME}"    
+    ziping="; zip -0 -r export-artifacts/export-pack-${zippostfix}.zip ${targetDirPck};"
 fi
 
-if [ "${PLATFORM_EXPORT}x" != "x" && "${PLATFORM_EXPORT}x" != "falsex" ]; then
-    godot_args="${godot_args} --export ${PLATFORM} ./export-platform"
-    ziping="; zip -0 -r export-artifacts/export-${zippostfix}.zip ./export-platform;"
+if [ "${PLATFORM_EXPORT}x" != "x" ] && [ "${PLATFORM_EXPORT}x" != "falsex" ]; then
+    godot_args="${godot_args} --export ${PLATFORM} ${targetDirPlatform}/${EXECNAME}"
+    ziping="; zip -0 -r export-artifacts/export-${zippostfix}.zip ${targetDirPlatform};"
 fi
+echo "::endgroup::"
 
 chmod +x Godot*
-./Godot* ${godot_args} --no-window ${targetDir}/project.godot
+echo "::group::running the engine with following parameters: ${godot_args}"
+./Godot* ${godot_args} --no-window ${targetDir}/project.godot --quit
 echo "::group::ziping projects..."
 ${ziping}
 echo "::endgroup::"
